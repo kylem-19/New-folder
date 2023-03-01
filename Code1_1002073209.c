d552986e47d6c12426ff6d4aa7f2624fb161cddf
kylem-19 <117118116+kylem-19@users.noreply.github.com>
2023-03-01 17:45:56 -0600

    go

--- Code1_1002073209.c
+++ Code1_1002073209.c
@@ -1,7 +1,7 @@
-// Kyle Moore 1002073209//
+//Kyle Moore//
+//1002073209//
 
 #include <stdio.h>
-
 int main (void)
 {
     printf("Your are making weekend plans and find out that your friends might visit. \n");
@@ -11,37 +11,40 @@ int main (void)
 
     printf("Your friends love to go to the movies, so if they visit, you will all go to the movies. \n");
     printf("But, if they don't visit, what will you do with your weekend? \n");
-    printf("Did your friends visit? \n");
     
     char decision;
-    scanf(" %c", &decision);
+
+    printf("Did your friends visit? \n");
+    scanf("%c", &decision);
 
     if (decision == 'Y')
     {
-       printf("Weekend plans - have fun at the movies with your friends! \n"); 
+        printf("Weekend plans - have fun at the movies with your friends!\n");
     }
     else
     {
-        printf("Your friends weren't able to visit so you need to make alternate plans \n");
+        printf("Your friends weren't able to visit so you need to make alternate plans. \n");
+
         printf("Is the weather nice? \n");
+        scanf(" %c", &decision);
 
-        scanf(" %c", &decision);
-    }
     if (decision == 'Y')
     {
-        printf("Weekend plans - ride bike in the park! \n");
+        printf("Weekend plans - ride bike in the park!\n");
     }
     else
     {
-        printf("Oh, so the weather is bad. Do you have enough money to go shopping? \n");
+        printf("Oh, so the weather is bad.  Do you have enough money to go shopping? \n");
         scanf(" %c", &decision);
-    }
+
     if (decision == 'Y')
     {
-        printf("Weekend plans - go shopping - don't spend too much! \n");
+        printf("Weekend plans - go shopping - don't spend too much!! \n");
     }
     else
     {
-        printf("Weekend plans - stay home and play video games!");
+        printf("Weekend plans - stay home and play video games!! \n");
+    }
+    }
     }
 }
