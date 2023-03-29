c89a5d58c6ddd3e52d4ede1cde437cf4a87ab972
Kyle <kmoore12004@gmail.com>
2023-03-29 17:10:51 -0500

    u

---
+++ cse-1310/code_projects/Code4_1002073209.c
@@ -0,0 +1,18 @@
+// Kyle Moore //
+// 1002073209 //
+#include <stdio.h>
+
+int main()
+{
+    char myChar;
+    printf("Enter the character you want to use ");
+    scanf("%c", myChar);
+}
+int GetValue()
+{
+    
+}
+int CheckValues()
+{
+
+}
