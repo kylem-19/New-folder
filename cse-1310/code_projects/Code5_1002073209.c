a6c3d8e90c636f911ca8379e7ddba4c3096aaa9f
Kyle <kmoore12004@gmail.com>
2023-04-17 18:25:48 -0500

    6:25 PM

--- cse-1310/code_projects/Code5_1002073209.c
+++ cse-1310/code_projects/Code5_1002073209.c
@@ -40,24 +40,110 @@ int HowManyFaces(int dice[], int SearchFace)
 }
 int main(void)
 {
-    int dice[];
-    int HowMany[];
+    int dice[NUMBEROFDICE];
+    int HowMany[NUMBEROFFACES];
     int numberOfRolls = 0, numberOfReRolls = NUMBEROFROLLS, ofAKind = 0, FullHouse = 0, TwoPair = 0, CSlam = 0, LargeStraight = 0, SmallStraight = 0, i = 0;
     char answer = 'N';
 
     do
     {
-        RollDice(dice[]);
+        RollDice(dice[i]);
         printf("You rolled");
-        PrintRoll(dice[]);
+        PrintRoll(dice[i]);
         numberOfRolls++;
-        for (size_t i = 0; i < count; i++)
+        for (i = 0; i < NUMBEROFFACES; i++)
         {
-            howMany[i] = HowManyFaces(dice, i+1);
+            HowMany[i] = HowManyFaces(dice, i+1);
         }
         
+        for ( i = 0; i < NUMBEROFFACES; i++)
+        {
+            if (HowMany[i] = 3)
+            {
+                FullHouse += 3;
+            }
+            if (HowMany[i] = 2)
+            {
+                FullHouse +=2;
+                TwoPair++;
+            }
+            if (HowMany[i] = NUMBEROFDICE)
+            {
+                CSlam++;
+            }
+            if (HowMany[i] = 1)
+            {
+                LargeStraight++;
+            }
+            else if (HowMany[i] = 0 && 5 < LargeStraight > 0)
+            {
+                LargeStraight = 0;
+            }
+            if (HowMany[i] >= 1)
+            {
+                SmallStraight++;
+            }
+            else if (HowMany[i] = 0 && 4 <SmallStraight > 0)
+            {
+                SmallStraight = 0;
+            }
+            if (HowMany[i] = 4)
+            {
+                ofAKind = 4;
+            }
+            if (HowMany[i] = 3)
+            {
+                ofAKind = 3;
+            }
+            
+        }
+        if (LargeStraight= 5)
+        {
+            printf("Large Straight");
+        }
+        else if (SmallStraight >= 4)
+        {
+            printf("Small Straight");
+        }
+        else if (FullHouse = 5)
+        {
+            printf("Full house");
+        }
+        else if (CSlam = 1)
+        {
+            printf("CSlam!");
+        }
+        else if (ofAKind = 4)
+        {
+            printf("Four of a kind!");
+        }
+        else if (ofAKind = 3)
+        {
+            printf("Three of a kind");
+        }
+        else if (TwoPair = 2)
+        {
+            printf("Two Pair");
+        }
+        else
+        {
+            printf("You have nothing");
+        }
+        if (numberOfRolls != NUMBEROFROLLS)
+        {
+            printf("Do yuo want to reroll?");
+            scanf("%c", &answer);
+
+            ofAKind = 0;
+            FullHouse = 0;
+            TwoPair = 0;
+            CSlam = 0;
+            LargeStraight = 0;
+            SmallStraight = 0;
+        }
 
     } while (answer == 'Y' && numberOfRolls < numberOfReRolls);
+    
     
 
 
