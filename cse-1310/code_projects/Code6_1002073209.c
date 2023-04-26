5d4f8a4aca49b2631767f90549d039846cd62cb9
kylem-19 <kmoore12004@gmail.com>
2023-04-26 13:53:16 -0500

    go

--- cse-1310/code_projects/Code6_1002073209.c
+++ cse-1310/code_projects/Code6_1002073209.c
@@ -1,11 +1,114 @@
-// Kyle Moore
-// 1002073209
-
+#include <stdio.h>
 #define MAX_ROWS 9
 #define MAX_COLS 9
 #define TICKET_LIMIT 4
 
-int main(void)
+void FillMovieTheater(char map[MAX_ROWS][MAX_COLS], int rows, int seats)
 {
-    char MovieTheater[MAX_ROWS][MAX_COLS] = {};
+    for (int i = 0; i < rows; i++)
+    {
+        for (int  j = 0; i < seats; j++)
+        {
+            map[i][j] = 'O';
+        }   
+    }
+}
+
+void PrintSeatMap(char map[MAX_ROWS][MAX_COLS], int rows, int seats)
+{
+    printf("\n    ");
+    for (int i = 0; i < seats; i++)
+    {
+        printf("%d ", i);
+    }
+    printf("\n");
+
+    for (int i = 0; i < rows; i++)
+    {
+        printf("%c ", 'A' + 1);
+        for (int j = 0; i < seats; j++)
+        {
+            printf("%c ", map[i][j]);
+        }   
+    }
+}
+
+int IsSeatSold(char map[MAX_ROWS][MAX_COLS], int rows, int cols)
+{
+    int seatcheck;
+
+    if (map[rows][cols] == 'X')
+    {
+        seatcheck = 1;
+    }
+    else
+    {
+        seatcheck = 0;
+        map[rows][cols] = 'X';
+    }
+    return seatcheck;
+}
+
+int main(void)
+{ 
+    char MovieTheater[MAX_ROWS][MAX_COLS];
+    int rows, cols, ticketsBought, ticketsSold;
+    int seatchoice;
+    char myRow;
+    do
+    {
+        printf("How many rows does your theater have? (1-%d) ", MAX_ROWS);
+        scanf("%d", &rows);
+
+        if (rows > MAX_ROWS || rows <= 1)
+        {
+            printf("Rows must be between 1 and %d. Please reenter. ", MAX_ROWS);
+            scanf("%d", &rows);
+        }
+    } while (rows > MAX_ROWS || rows < 1);
+    
+    do
+    {
+        printf("How many seats are there per row? (1-%d) ", MAX_COLS);
+        scanf("%d", &cols);
+
+        if (cols > MAX_COLS || cols < 1)
+        {
+            printf("Columns must be between 1 and %d. Please reenter.", MAX_COLS);
+            scanf("%d", &cols);
+        }
+    } while (cols > MAX_COLS || cols < 1);
+    
+    FillMovieTheater(MovieTheater, rows, cols);
+
+    printf("How many tickets would you like to purchase? (limit %d) ", TICKET_LIMIT);
+    scanf("%d", &ticketsBought);
+
+    while (ticketsBought > TICKET_LIMIT || ticketsBought < 0)
+    {
+        printf("This is a special showing - limit of %d tickets per purchase.\n", TICKET_LIMIT);
+        printf("How many tickets would you like to purchase? (limit %d) ", TICKET_LIMIT);
+        scanf("%d", &ticketsBought);
+    }
+
+    if (ticketsBought == 0)
+    {
+        printf("No movie for you!\n");
+    }
+    else
+    {
+        while (ticketsSold < ticketsBought)
+        {
+            PrintSeatMap(MovieTheater, rows, cols);
+
+            printf("Enter seat choice by entering the row and seat ");
+            printf("Please pick a seat ");
+            scanf("%c%d", &myRow, &seatchoice);
+
+
+
+        }
+        
+    }
+    
 }
--- temp.c
+++ temp.c
@@ -1,102 +1,34 @@
+#define ROW 100
+#define COL 400
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-
-#define NUMBEROFREROLLS 3
-#define NUMBEROFFACES 6
-#define NUMBEROFDICE 5
-
-void RollDice(int dice[]);
-void PrintRoll(int dice[]);
-int HowManyFaces(int dice[], int searchFace);
-
-int main() {
-    int dice[NUMBEROFDICE];
-    int howMany[NUMBEROFFACES];
-    int numberOfRolls = 0;
-    int numberOfReRolls = NUMBEROFREROLLS;
-    char answer = 'N';
-    int ofAKind = 0;
-    int FullHouse = 0;
-    int TwoPair = 0;
-    int CSlam = 0;
-    int LargeStraight = 0;
-    int SmallStraight = 0;
-    int i = 0;
-
-    srand(time(NULL)); // initialize random number generator
-
-    do {
-        RollDice(dice);
-        PrintRoll(dice);
-        numberOfRolls++;
-
-        // Count number of faces for each die roll
-        for (i = 0; i < NUMBEROFFACES; i++) {
-            howMany[i] = HowManyFaces(dice, i+1);
+void PrintIt(int My2D[][COL])
+{
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int j = 0; j < COL; j++)
+        {
+            printf("%5d", My2D[i][j]);
         }
+        printf("\n");
+    }
+}
 
-        // Check for different combinations
-        for (i = 0; i < NUMBEROFFACES; i++) {
-            if (howMany[i] == 3) {
-                FullHouse += 3;
-            }
-            if (howMany[i] == 2) {
-                FullHouse += 2;
-                TwoPair++;
-            }
-            if (howMany[i] == NUMBEROFDICE) {
-                CSlam++;
-            }
-            if (howMany[i] == 1) {
-                LargeStraight++;
-            } else if (howMany[i] == 0 && LargeStraight > 0 && LargeStraight < 5) {
-                LargeStraight = 0;
-            }
-            if (howMany[i] >= 1) {
-                SmallStraight++;
-            } else if (howMany[i] == 0 && SmallStraight > 0 && SmallStraight < 4) {
-                SmallStraight = 0;
-            }
-            if (howMany[i] == 4) {
-                ofAKind = 4;
-            }
-            if (howMany[i] == 3) {
-                ofAKind = 3;
-            }
-        }
-
-        // Print the result of each roll
-        if (LargeStraight == 5) {
-            printf("Large Straight\n");
-        } else if (SmallStraight >= 4) {
-            printf("Small Straight\n");
-        } else if (FullHouse == 5) {
-            printf("Full House\n");
-        } else if (CSlam == 1) {
-            printf("CSlam!!\n");
-        } else if (ofAKind == 4) {
-            printf("Four of a kind\n");
-        } else if (ofAKind == 3) {
-            printf("Three of a kind\n");
-        } else if (TwoPair == 2) {
-            printf("Two Pair\n");
-        } else {
-            printf("You have nothing\n");
-        }
-
-        if (numberOfRolls != NUMBEROFREROLLS) {
-            printf("Do you want to reroll? (Y/N) ");
-            scanf(" %c", &answer);
-            }
+void FillIt(int my2D[][COL])
+{
+    for (int i = 0; i < ROW; i++)
+    {
+        for (int j = 0; j < COL; j++)
+        {
+            my2D[i][j] = (i-1)*(j+1);
         }
     }
-    while (answer == 'Y' || answer == 'y') {
-                ofAKind = 0;
-                FullHouse = 0;
-                TwoPair = 0;
-                CSlam = 0;
-                LargeStraight = 0;
-                SmallStraight = 0;
-   
+}
+
+int main(void)
+{
+    int my2D[ROW][COL] = {};
+
+    FillIt(my2D);
+    PrintIt(my2D);
+    printf("%d", my2D[65][367]);
 }
