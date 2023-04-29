e71c7f0c5beb16107a6ce534a10d02c3bdc5a66d
kylem-19 <kmoore12004@gmail.com>
2023-04-28 19:05:44 -0500

    eral

--- arrays.c
+++ arrays.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
 
-int main(void)
+void printarray(int array[][], int rows, int cols)
 {
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < cols; i++)
+        {
+            array[i][j];
+        }
+        
+    }
     
 }
