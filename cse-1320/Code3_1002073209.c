1250e42c5679a2d544aef57c21a051e76ac4fc8c
kylem-19 <kmoore12004@gmail.com>
2023-10-07 16:26:32 -0500

    started

---
+++ cse-1320/Code3_1002073209.c
@@ -0,0 +1,43 @@
+// Kyle Moore 1002073209
+#include <stdio.h>
+#include <time.h>
+
+
+void printcard();
+int fillcard();
+int picknumber();
+int checkrow();
+int checkcolumn();
+int checknumb();
+
+
+
+
+
+int checknumb(int card[][5], int realnumb)
+{
+    for (int i = 0; i < card; i++)
+    {
+        /* code */
+    }
+    
+}
+
+int fillcard(int card[5][5])
+{
+    int bingocard[5][5] = {};
+
+
+}
+
+void printcard(int bingo[5][5])
+{
+    
+}
+
+int main(void)
+{
+    int counter;
+    int bingocard[5][5] = {};
+    printcard(bingocard);
+}
