d6b0a620982a30c63e4bffc61c916fc3686e4946
kylem-19 <kmoore12004@gmail.com>
2023-10-28 14:55:09 -0500

    cd4

--- cse-1320/Code4/Code4_1002073209.c
+++ cse-1320/Code4/Code4_1002073209.c
@@ -1,18 +1,31 @@
 #include "DrawTool.h"
+#include <string.h>
 
 
 int main(void)
 {
-    char drawcmd1;
+    char drawcmd1[20];
     int user_size = 0;
     char large_array[MAXMAPSIZE][MAXMAPSIZE] = {};
-
+    char *token = NULL;
+    char *Delimiters = "(,)";
     InitializeMap(large_array, &user_size);
     PrintInstructions();
     PrintMap(large_array, user_size);
 
     printf("Enter draw command (Enter Q to quit) \n");
-    scanf("%c", &drawcmd1);
-    fgets(drawcmd1, 21-1 ,stdin)
+    scanf("%c", drawcmd1);
+    fgets(drawcmd1, 20 ,stdin);
+    token = strtok(drawcmd1, Delimiters);
+    strcpy(, token);
+    
+    while (drawcmd1 != "Q" || "q")
+    {
+        /* code */
+    }
+    
+    
+    
+    
     
 }
