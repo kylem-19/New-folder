0d546eff0016ca71e1c75437bc0f642d8cb5c27c
kylem-19 <kmoore12004@gmail.com>
2023-10-30 01:09:45 -0500

    Getting "2nd" strtok from cmd input

--- cse-1320/Code4/Code4_1002073209.c
+++ cse-1320/Code4/Code4_1002073209.c
@@ -1,30 +1,40 @@
 #include "DrawTool.h"
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 
 int main(void)
 {
-    char drawcmd1[20];
+    char drawcmd1[21];
     int user_size = 0;
     char large_array[MAXMAPSIZE][MAXMAPSIZE] = {};
     char *token = NULL;
-    char *Delimiters = "(,)";
+    char *Delimiters = "(),";
+    char Lettercmd[20] = {};
+    int letter;
+    char userRows[20] = {};
+    int Rows;
     InitializeMap(large_array, &user_size);
     PrintInstructions();
     PrintMap(large_array, user_size);
 
     printf("Enter draw command (Enter Q to quit) \n");
-    scanf("%c", drawcmd1);
-    fgets(drawcmd1, 20 ,stdin);
+    fgets(drawcmd1, 21 ,stdin);
+    drawcmd1[strlen(drawcmd1) -1] = '\0';
+    toupper(drawcmd1[0]);
     token = strtok(drawcmd1, Delimiters);
-    strcpy(, token);
-    
-    while (drawcmd1 != "Q" || "q")
+    strcpy(Lettercmd,token);
+    letter = atoi(Lettercmd);
+
+    while (Lettercmd != "C")
     {
-        /* code */
+        token = strtok(NULL, Delimiters);
+        strcpy(userRows, token);
+        Rows = atoi(userRows);
     }
     
-    
+
     
     
     
