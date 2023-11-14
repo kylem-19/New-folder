7552999e57380e04f96073999b5d960305206c64
kylem-19 <kmoore12004@gmail.com>
2023-11-14 16:34:25 -0600

    s

---
+++ cse-1320/linkedlists.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char argv[])
+{
+    struct list
+    {
+        struct list *ptr;
+        int Apple;
+        char Banana;
+        char Carrots[20];
+    };
+    struct list mylist = {};
+
+    char *tempPtr;
+    tempPtr = mylist.ptr;
+
+    if (tempPtr == NULL)
+    {
+        return;
+    }
+    while (tempPtr != NULL)
+    {
+        printf("not null -- new list\n");
+        
+    }
+    
+    
+}
