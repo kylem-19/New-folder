38d2ea8f23da81cdccd3d8d029ba94d90c010085
kylem-19 <kmoore12004@gmail.com>
2023-11-14 13:10:32 -0600

    gu

--- cse-1320/OLQ11.c
+++ cse-1320/OLQ11.c
@@ -1,14 +1,47 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
+#include <stdlib.h>
 
-int main(int argc, char argv[])
+int main(void)
 {
-    char dog[100] = "efghudhuiogerfhuiobgjinximopsdaop";
-    printf("%c", *(dog+5));
-    printf("%c", *(strpbrk(dog, "gdi")+9));
-    printf("%c", (int)strlen(dog)+65);
-    printf("%d", abs(dog - strstr(dog, "obg")));
+    // char greeting[] ={"Hello there-How are you&I am fine*"};
+    // char phrase1[20] = {};
+    // char phrase2[20] = {};
+    // char phrase3[20] = {};
 
+    // char *token;
+    // char delimiters[] = "-,&,*";
+
+    // token = strtok(greeting, delimiters);
+    // strcpy(phrase1, token);
+    // token = strtok(NULL, delimiters);
+    // strcpy(phrase2, token);
+    // token = strtok(NULL, delimiters);
+    // strcpy(phrase3, token);
+
+    // printf("%s %s %s", phrase2, phrase3, phrase1);
+    // return 0;
+
+
+    //char alpha[] = "Supercalifragilisticexpialidocious";
+    // printf("%c", *(alpha+7));
+    // printf("%c", *(strpbrk(alpha, "dzq")+4));
+    //printf("%c", (int)strlen(alpha)*2);
+    // printf("%d", abs(alpha - strstr(alpha, "list")));
+
+
+    struct car
+    {
+        int mph;
+        float price;
+        char model;
+        char body[100];
+    };
+    struct car mycar = {};
     
+
+
+
 }
+
+
